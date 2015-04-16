475337c0ef114bbc43618d0e645ac769e1447ac7
mangledjambon <MotorHead666>
2015-04-16 10:46:07 +0100

    fixed include issues when importing files

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
-#include "FFTReal.hpp"
+#include "./fft/FFTReal.h"
 
 //class to compute FFT using generic programming
 
 using namespace std;
-using namespace ffft;
 
 void do_fft();
 
 int main() {
 
     float x[5] = {2.3f, 4.5f, 0.546f, 2.3415f, 1.21312314f};
-
-    //
 
     return 0;
 }
