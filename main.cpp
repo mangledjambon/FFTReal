61ed7b178a1a836b7e0a07ece5b43d24c64d1a25
mangledjambon <MotorHead666>
2015-04-16 10:15:35 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "FFTReal.hpp"
+
+//class to compute FFT using generic programming
+
+using namespace std;
+using namespace ffft;
+
+void do_fft();
+
+int main() {
+
+    float x[5] = {2.3f, 4.5f, 0.546f, 2.3415f, 1.21312314f};
+
+    //
+
+    return 0;
+}
